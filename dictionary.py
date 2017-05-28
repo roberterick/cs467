45c1f620c832f2d28ca6237a09990cbe35e38977
jamesjwong <wongjam@oregonstate.edu>
2017-05-28 16:13:15 -0400

    Parser update (special characters)
    Updated parser so that i removes special characters and punctuation from
    user input.

--- dictionary.py
+++ dictionary.py
@@ -1,11 +1,18 @@
+##class: CS467
+##group: Sagitta - Parser/Dictionary
+##members: Robert Erick, James Wong, Brent Nolan
+##date: 4/17/2017
+
 #list of our primary verbs
 possibleVerbs = ['move', 'examine']
 
+# --MOVE--
 #possible variants of the word move
 moveVariants = ['moving', 'moved', 'go', 'walk']
 for b in moveVariants:
 	possibleVerbs.append(b)
 
+# --DIRECTIONS--
 possibleDirections = ['north','south', 'east', 'west']
 #possible variants of the different directions
 northVariants = ['up']
@@ -13,12 +20,30 @@ eastVariants = ['right']
 southVariants = ['down']
 westVariants = ['left']
 
+# --PREPOSITIONS--
 possiblePrepositions = []
 
+# --SPECIAL CHARACTERS--
+#possible special characters that should be removed from words in 
+#userinput
+possibleSpecialChars = " ?.!/;:,"
+
+#sagParser hopefully returns a list that will list at 
+# [0] - action, then [1] - direction/object
 def sagParser(userInput):
 	inputList = userInput.split(' ')
-	#sagParser hopefully returns a list that will list at 
-	# [0] - action, then [1] - direction/object
+	
+	#remove special characters from strings in userInputArray
+	#based off of: 
+	#https://stackoverflow.com/questions/3939361/remove-specific-characters-from-a-string-in-python
+	for line in inputList:
+		for char in line:
+		    if char in possibleSpecialChars:
+		        line1 = line.replace(char,'')
+		        inputList.remove(line)
+		        inputList.append(line1)
+
+	#list of returned strings
 	parserReturn = []
 	foundVerb = verbFinder(inputList)
 	if foundVerb == 'move':
@@ -38,7 +63,7 @@ def verbFinder(inputList):
 					verbCounter += 1
 					foundVerb = n
 	if verbCounter > 1:
-		print 'Your input has too many verbs'
+		print 'Your input has too many verbs.'
 	if verbCounter < 1: 
 		print 'There are not enough verbs.'
 	if verbCounter == 1:
@@ -58,7 +83,7 @@ def directionFinder(inputList):
 					directionCounter += 1
 					foundDirection = n
 	if directionCounter > 1:
-		print 'Your input has too many directions'
+		print 'Your input has too many directions.'
 	if directionCounter < 1: 
 		print 'There are not enough directions.'
 	if directionCounter == 1:
